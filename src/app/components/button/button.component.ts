a88edb81fe367834bbd8fbfd4bf50833a4317b50
Carlo <you@example.comcarlo.diiorio@si2001.it>
2025-02-24 16:58:04 +0100

    riorganizzazione button custom

---
+++ src/app/components/button/button.component.ts
@@ -0,0 +1,18 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { MyActions } from '../my-table/my-table-config';
+
+@Component({
+  selector: 'app-button',
+  templateUrl: './button.component.html',
+  styleUrl: './button.component.css'
+})
+export class ButtonComponent {
+  @Input() actionRichiesta: MyActions | undefined;
+  @Input() addButton?: boolean;
+  @Output() onClick = new EventEmitter<any>();
+
+  handleClick(){
+    this.onClick.emit(this.actionRichiesta?.label)
+  }
+
+}
