d68aace17d243ab4fecc07d334a33c8183922470
Carlo <you@example.comcarlo.diiorio@si2001.it>
2025-02-24 16:57:10 +0100

    riorganizzazione

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,17 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HomepageComponent } from './homepage/homepage.component';
-import { ParcoAutoComponent } from './parco-auto/parco-auto.component';
-import { ProfiloUtenteComponent } from './profilo-utente/profilo-utente.component';
-import { PrenotazioniComponent } from './prenotazioni/prenotazioni.component';
-import { LoginPageComponent } from './login-page/login-page.component';
+import { HomepageComponent } from './components/homepage/homepage.component';
+import { ParcoAutoComponent } from './components/parco-auto/parco-auto.component';
+import { ProfiloUtenteComponent } from './components/profilo-utente/profilo-utente.component';
+import { PrenotazioniComponent } from './components/prenotazioni/prenotazioni.component';
+import { LoginPageComponent } from './components/login-page/login-page.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: '/homepage', pathMatch: 'full' },
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
   { path: 'login', component: LoginPageComponent },
   { path: 'homepage', component: HomepageComponent },
   { path: 'parco-auto', component: ParcoAutoComponent },
-  { path: 'profilo-utente/:id', component: ProfiloUtenteComponent },
+  { path: 'dettagli-utente/:id', component: ProfiloUtenteComponent },
+  { path: 'aggiungi-utente', component: ProfiloUtenteComponent},
   { path: 'prenotazioni', component: PrenotazioniComponent}
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,29 +3,29 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavHeadersComponent } from './nav-headers/nav-headers.component';
-import { ParcoAutoComponent } from './parco-auto/parco-auto.component';
-import { HomepageComponent } from './homepage/homepage.component';
-import { PrenotazioniComponent } from './prenotazioni/prenotazioni.component';
-import { ProfiloUtenteComponent } from './profilo-utente/profilo-utente.component';
-import { ButtonComponent } from './button/button.component';
-import { MyTableComponent } from './my-table/my-table.component';
+import { NavHeadersComponent } from './components/nav-headers/nav-headers.component';
+import { ParcoAutoComponent } from './components/parco-auto/parco-auto.component';
+import { HomepageComponent } from './components/homepage/homepage.component';
+import { PrenotazioniComponent } from './components/prenotazioni/prenotazioni.component';
+import { ProfiloUtenteComponent } from './components/profilo-utente/profilo-utente.component';
+import { ButtonComponent } from './components/button/button.component';
+import { MyTableComponent } from './components/my-table/my-table.component';
 import { PaginazionePipe } from './paginazione.pipe';
 import { FormsModule } from '@angular/forms';
-import { LoginPageComponent } from './login-page/login-page.component';
-import { AutenticazioneService } from './autenticazione.service';
+import { LoginPageComponent } from './components/login-page/login-page.component';
+import { AutenticazioneService } from './services/login/autenticazione.service';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavHeadersComponent,
-    ParcoAutoComponent,
-    HomepageComponent,
     PrenotazioniComponent,
-    ProfiloUtenteComponent,
     PaginazionePipe,
     ButtonComponent,
     MyTableComponent,
+    ProfiloUtenteComponent,
+    HomepageComponent,
+    ParcoAutoComponent,
     LoginPageComponent
   ],
   imports: [
